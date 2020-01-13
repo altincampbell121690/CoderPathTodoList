f80bfa2727f17d8f1ccbe5f81586e1f36f209278
altin <altincampbell121690@gmail.com>
2020-01-12 21:02:49 -0800

    intit

--- app/src/main/java/com/example/coderpathtodolist/Adapter/todoAdapter.kt
+++ app/src/main/java/com/example/coderpathtodolist/Adapter/todoAdapter.kt
@@ -2,17 +2,31 @@ package com.example.coderpathtodolist.Adapter
 
 import android.content.Context
 import android.view.ContextMenu
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.coderpathtodolist.R
+import kotlinx.android.synthetic.main.list_item.view.*
 
 class todoAdapter(val context: Context, val todoList : List<String>) : RecyclerView.Adapter<todoAdapter.todoHolder>() {
     inner class todoHolder(itemView:View) : RecyclerView.ViewHolder(itemView){
+        val listItem = itemView.findViewById<TextView>(R.id.listItemtextView)
+
+
+        fun bindItem(item:String, context: Context){
+            listItem.text = item
+
+        }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): todoHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+       val view = LayoutInflater.from(parent.context).inflate(
+           R.layout.list_item,parent, false
+       )
+        return todoHolder(view)
     }
 
     override fun getItemCount(): Int {
@@ -20,6 +34,6 @@ class todoAdapter(val context: Context, val todoList : List<String>) : RecyclerV
     }
 
     override fun onBindViewHolder(holder: todoHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.bindItem(todoList[position], context)
     }
 }
--- app/src/main/java/com/example/coderpathtodolist/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderpathtodolist/Controller/MainActivity.kt
@@ -1,13 +1,14 @@
-package com.example.coderpathtodolist
+package com.example.coderpathtodolist.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.example.coderpathtodolist.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var todoList: MutableList<String>
+    var todoList =  mutableListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,11 +18,12 @@ class MainActivity : AppCompatActivity() {
 
 
     fun onAddListItem(view:View){
-        if(editListText.text != null) {
+        if(editListText.text.toString() != "") {
             todoList.add(editListText.text.toString())
             Toast.makeText(this, "item $todoList", Toast.LENGTH_SHORT).show()
         }
         else
             Toast.makeText(this,"Please Enter Text", Toast.LENGTH_SHORT).show()
     }
+
 }
